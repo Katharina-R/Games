f6dc1f159642bf2fe6369a5363dcebe237de37ed
Katharina Desktop <ga83pad@mytum.de>
2020-04-14 20:29:14 +0200

    Added colours for setStyle

---
+++ src/GUIColour.java
@@ -0,0 +1,6 @@
+public class GUIColour {
+
+    public static final String GAME_INFO = "-fx-background-color: #79b589;";
+    public static final String CELL = " -fx-background-color: #2E64E8;";
+    public static final String UI = "-fx-background-color: #dce3f5;";
+}
