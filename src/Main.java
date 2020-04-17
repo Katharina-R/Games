c2297d0189baea6a9fe5246555d37e3b8b31b651
Katharina Desktop <ga83pad@mytum.de>
2020-04-17 20:29:50 +0200

    Game is playable in PvP mode

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -50,41 +51,73 @@ public class Main extends Application {
         notify(); // notify gameThread in getPlayerEvent
     }
 
+    private int getPlayerMove() throws InterruptedException{
+        int move;
+
+        do{
+            getPlayerEvent(0);
+
+            if(playerEvent != PlayerEvent.MOVE) return -1;
+
+            move = lastClicked;
+
+        } while(!game.isValidMove(move));
+
+        return move;
+    }
+
+    private void updateGUI(){
+
+        for(int x = 0; x < Game.N; x++){
+            for(int y = 0; y < Game.M; y++){
+                if(game.getStone(x, y) == Game.YELLOW) gui.setStone(x + 1, y, Paint.valueOf(Style.YELLOW));
+                else if(game.getStone(x, y) == Game.RED) gui.setStone(x + 1, y, Paint.valueOf(Style.RED));
+                else gui.setStone(x + 1, y, Paint.valueOf(Style.EMPTY));
+            }
+        }
+    }
+
     private void gameLoop(){
         System.out.println("started game loop");
 
         // init game
         game = new Game();
 
+        int move;
+
         while(!game.isOver()){
 
-            if(isPlayerMove()){
+            // TODO: mark possible moves
 
+            if(isPlayerMove()){
                 try {
-                    getPlayerEvent(0);
+                    move = getPlayerMove();
                 } catch (InterruptedException e) {
-                    System.out.println("Stopping Game Thread");
                     return;
                 }
 
-                if(playerEvent == PlayerEvent.MOVE){
-                    //TODO: move
-                }
-                else if(playerEvent == PlayerEvent.NEW_GAME){
+                if(playerEvent == PlayerEvent.NEW_GAME){
                     //TODO: new game
+                    break;
                 }
                 else if(playerEvent == PlayerEvent.CHANGE_GAME_MODE){
                     // TODO: change game mode
+                    continue;
                 }
                 else if(playerEvent == PlayerEvent.UNDO){
                     // TODO: undo
+                    continue;
                 }
+
+                game.makeMove(move);
             }
             else{
 
                 // TODO: get ai move
                 //TODO: set stone
             }
+
+            updateGUI();
         }
     }
 
