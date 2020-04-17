9119ed009c2709c806ed4cbfd36bb582a963f389
Katharina Desktop <ga83pad@mytum.de>
2020-04-17 18:29:24 +0200

    Store current player

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,18 @@ public class Game {
 
     public static final int N = 6; // rows
     public static final int M = 7; // columns
+
+    public static final int YELLOW = -1;
+    public static final int RED = 1;
+
+    private int player = YELLOW;
+
+    public int getCurrentPlayer(){
+        return player;
+    }
+
+    public boolean isOver(){
+        //TODO: implement isOver
+        return false;
+    }
 }
