1e3a59d40c9a30adb4f703318358733b087aca48
Katharina Reuschel <29693196+Katharina-R@users.noreply.github.com>
2022-01-11 20:49:09 +0000

    Hangman

---
+++ Hangman/alphabet.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <locale>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum Used {YES, NO, MAYBE}; 
+
+const vector<wchar_t> ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+const wstring RED = L"\033[31m";
+const wstring BLACK = L"\033[0m";
+const wstring GREEN = L"\033[32m";
+
+class Alphabet {
+ private:
+  map<wchar_t, Used> chars;
+  map<wchar_t, Used> specialChars;
+
+ public:
+  Alphabet(wstring specialChars_){
+  	for(wchar_t c : ALPHABET) chars[c] = Used::MAYBE;
+  	for(wchar_t c : specialChars_) {
+  		c = towupper(c);
+  		if(!isValid(c)){
+  			specialChars[c] = Used::MAYBE;
+  		}
+  	}
+  }
+
+	bool isValid(const wchar_t c_unsafe) const {
+		wchar_t c = towupper(c_unsafe);
+		if(chars.contains(c) || specialChars.contains(c)) return true;
+
+		wcout << c << " is not a valid character!" << endl;
+		return false;
+	}
+
+	bool isValid(const wstring& word) const{
+		bool valid = true;
+		for(wchar_t c_unsafe : word){
+			wchar_t c = towupper(c_unsafe);
+			if(!isValid(c)) {
+				valid = false;
+			}
+		}
+		return valid;
+	}
+
+	void mark(wchar_t c_unsafe, Used used){
+		wchar_t c = towupper(c_unsafe);
+		if(chars.contains(c)) chars[c] = used;
+		if(specialChars.contains(c)) specialChars[c] = used;
+	}
+
+	Used isUsed(const wchar_t c_unsafe) const{
+		wchar_t c = towupper(c_unsafe);
+		if(chars.contains(c)) return chars.at(c);
+		return specialChars.at(c);
+	}
+
+	void clearUsed() {
+		for(auto& p : chars) p.second = Used::MAYBE;
+		for(auto& p : specialChars) p.second = Used::MAYBE;
+	}
+
+	wstring colour(const Used& used) const{
+		switch(used){
+			case YES: return GREEN;
+			case NO: return RED;
+			default: return BLACK;
+		}
+	}
+
+	void print() const{
+		wcout << "The alphabet is: ";
+		for(const auto& p : chars) wcout << colour(p.second) << p.first << " ";
+		for(const auto& p : specialChars) wcout << colour(p.second) << p.first << " ";
+		wcout << BLACK << endl;
+	}
+};
---
+++ Hangman/main.cpp
@@ -0,0 +1,146 @@
+#include "alphabet.cpp"
+
+using namespace std;
+
+void clearConsole(){
+	wcout << "\x1B[2J\x1B[H";
+}
+
+bool isUserConfirm(){
+	wstring confirm;
+	wcin >> confirm;
+	return confirm.compare(L"yes") == 0 || confirm == L"y";
+}
+
+Alphabet getAlphabet(){
+	wcout << "The default alphabet is: ";
+	for(wchar_t c : ALPHABET) wcout << c << " ";
+	wcout << endl;
+
+	wcout << "Please enter all special characters you want to add: ";
+	wstring specialChars;
+	getline(wcin, specialChars);
+
+	Alphabet alphabet(specialChars); 
+	alphabet.print();
+
+	return alphabet;
+}
+
+wstring getSecretWord(const Alphabet& alphabet){
+	wstring secretWord;
+
+	while(true) {
+		// Read secret word
+		wcout << "\n\nChoose a secret word: ";
+		wcin >> secretWord;
+		transform(secretWord.begin(), secretWord.end(), secretWord.begin(), towupper);
+		
+		// Check secret word
+		if(!alphabet.isValid(secretWord)){
+			alphabet.print();
+			continue;
+		}
+
+		// Ask user to confirm
+		wcout << "Your secret word is " << secretWord << ". Is that correct (yes/no)? ";
+		if(isUserConfirm()) break;
+	}
+
+	clearConsole();
+	wcout << "A secret word has been chosen!" << endl;
+
+	return secretWord;
+}
+
+void printSecretWord(const wstring& secretWord, const Alphabet& alphabet){
+	wcout << "The secret word is: ";
+	for(wchar_t c : secretWord){
+		if(alphabet.isUsed(c) == Used::YES){
+			wcout << c << " ";
+		}
+		else {
+			wcout << "_ ";
+		}
+	}
+	cout << endl;
+}
+
+wchar_t getLetter(const Alphabet& alphabet){
+	wchar_t letter;
+
+	while(true){
+		wcout << "\n\nChoose a letter: ";
+		wcin >> letter;
+
+		if(alphabet.isValid(letter)) break;
+	}
+
+	return letter;
+}
+
+bool isGameOver(int lives, const wstring& secretWord, const Alphabet& alphabet){
+	// Game lost
+	if(lives < 1){
+		wcout << "You lost!" << endl;
+		return true;
+	}
+
+	// Game won
+	for(wchar_t c : secretWord){
+		if(alphabet.isUsed(c) != Used::YES) return false;
+	}
+
+	// Game in progress
+	wcout << "You won!" << endl;
+	return true;
+}
+
+bool isPlayAgain(){
+	wcout << "Do you want to play again (yes/no)? ";
+	return isUserConfirm();
+}
+
+// g++ -std=gnu++2a -o main main.cpp
+int main(){
+	// Set up locale
+	ios_base::sync_with_stdio(false);
+	locale::global(locale("en_US.UTF8"));
+	wcin.imbue(locale());
+    wcout.imbue(locale());
+
+	// Set up alphabet
+	Alphabet alphabet = getAlphabet();
+
+	while(true){
+		int lives = 10;
+		wstring secretWord = getSecretWord(alphabet);
+
+		// Game loop
+		for(int i = 1; ; i++) {
+			wcout << "\n\nRound " << i << endl;
+			wcout << "You have " << lives << L" hearts" << endl;
+			alphabet.print();
+			printSecretWord(secretWord, alphabet);
+
+			wchar_t letter = getLetter(alphabet);
+
+			if(secretWord.find(towupper(letter)) != wstring::npos) {
+				alphabet.mark(letter, Used::YES);
+			}
+			else {
+				lives--;
+				alphabet.mark(letter, Used::NO);
+			}
+
+			if(isGameOver(lives, secretWord, alphabet)) {
+				wcout << "The secret word was " << secretWord << endl;
+				break;
+			}
+		}
+
+		if(!isPlayAgain()) break;
+		clearConsole();
+		alphabet.clearUsed();
+	}
+}
